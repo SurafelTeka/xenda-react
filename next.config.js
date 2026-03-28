55298519de41ee4ac5593a3c0d735e2332fbd0db
SurafelTeka <se.surafel.teka@gmail.com>
2026-03-28 21:22:19 +0800

    fixed env

--- next.config.js
+++ next.config.js
@@ -1,34 +1,47 @@
-/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    swcPlugins: [],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production' ? {
+      exclude: ['error', 'warn'], // Keep error and warn, remove log, info, debug, etc.
+    } : false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '**', // allows all https domains
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**', // allows all https domains
+        pathname: '/**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
+
+
+
+// /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   reactStrictMode: true,
 //   images: {
 //     domains: [
-//       "www.xenda.online",
-//       "xenda.online",
-//       "cameroun.xenda.online",
+//       "bjorn66.com",
+//       "6ammart-test.6amdev.xyz",
+//       "192.168.50.168",
+//       "6ammart-dev.6amdev.xyz",
 //     ], // Add the domain here
 //   },
 // };
-
+//
 // module.exports = nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  productionBrowserSourceMaps: true,
-  images: {
-    domains: [
-      "www.xenda.online",
-      "xenda.online",
-      "cameroun.xenda.online",
-    ],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias['@sentry/node'] = '@sentry/browser';
-    }
-    return config;
-  },
-};
-
-module.exports = nextConfig; // 👈 Removed analyzer
