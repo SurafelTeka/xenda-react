47ef5ea76a3d4da78ff802fdf5f965ceb4100ade
SurafelTeka <se.surafel.teka@gmail.com>
2026-04-05 08:55:26 +0800

    token fcm  issue

--- src/api-manage/hooks/react-query/push-notifications/usePushNotification.js
+++ src/api-manage/hooks/react-query/push-notifications/usePushNotification.js
@@ -2,8 +2,16 @@ import { useMutation } from "react-query";
 import MainApi from "../../../MainApi";
 import { cm_firebase_token_api } from "../../../ApiRoutes";
 import { onSingleErrorResponse } from "../../../api-error-response/ErrorResponses";
+import { getToken } from "../../../../helper-functions/getToken";
 
 const postHandler = async (token) => {
+  const userToken = getToken();
+  if (!userToken) {
+    throw new Error("User authentication token is required for FCM registration");
+  }
+  if (!token) {
+    throw new Error("FCM token is required");
+  }
   const { data } = await MainApi.post(`${cm_firebase_token_api}`, {
     cm_firebase_token: token,
     _method: "put",
@@ -13,5 +21,6 @@ const postHandler = async (token) => {
 export const useStoreFcm = () => {
   return useMutation("fcm_token", postHandler, {
     onError: onSingleErrorResponse,
+    retry: false,
   });
 };
